3e91dd19efbf4b046c4f07d5d19a4226890ab94d
Maick Duarte <93270459+MaickDuarte@users.noreply.github.com>
2022-07-24 19:59:54 -0300

    Adicionando seção "Sobre"
    Aqui eu adicionei uma seção onde contem informações cobre mim. Eu dividi ela em duas partes, esquerda com 1/3 e direita com 2/3.
    Na parte esquerda eu coloquei uma foto minha, um botão que faz o download do meu currículo, outro que te direciona ao meu Email, e mais um que te direciona a um link com meus certificados.
    Na parte direita, eu comecei colocando um titulo "SOBRE MIM", e depois um texto com uma introdução a mim. Após isso, coloquei um hr para fazer uma divisória e deixei dois botões, "PRINCIPAIS HABILIDADES" e "EXPERIENCIAS PROFISSIONAIS".
    Ao clicar em principais habilidades, vai aparecer duas ULS, com alguns dos conhecimentos que eu tenho, e o titulo vai ficar com um sublinhado roxo.
    Ao clicar em experiências profissionais, vai aparecer mais duas opções, "JOVEM APRENDIZ" e "TECNICO DE SUPORTE EM TI", o titulo vai ficar com um sublinhado roxo.
    Automaticamente, vai aparecer minha primeira experiência profissional, porém você pode clicar na outra para aparecer as informações dela, a experiência selecionada ficará com um sublinhado roxo. Eu coloquei o nome da empresa, período em que trabalhei nela e uma descrição das minhas funções.
    
    É A PRIMEIRA VEZ QUE EU INTRODUZO JAVASCRIPT EM MEU PORTFOLIO.
    Portanto não tenho tanta experiência, e talvez o código ficou um pouco repetitivo, mas eu gostei do resultado.
    
    Eu venho estudado bastante sobre responsividade, e percebi que a parte do HOME está com vários bugs, um dos meus próximos passos vai ser reformular totalmente o inicio da pagina, procurando evitar bugs

---
+++ js.sobre.js
@@ -0,0 +1,64 @@
+/* 
+padrao: lista-habilidades e menu-experiencias com style="display: none;"
+
+ao clicar em 'PRINCIPAIS HABILIDADES' 
+- escolha = display:none;
+- lista-habilidades = display: flex;
+- menu-experiencias = display: none;
+
+
+ao clicar em EXPERIENCIAS PROFISSIONAIS
+- escolha = display:none;
+- lista-habilidades = display: none;
+- menu-experiencias = display: flex;
+
+
+ao clicar em JOVEM APRENDIZ
+- jovem-experiencia = display: flex;
+- tecnico-experiencia = display: none;
+
+ao clicar em TECNICO DE SUPORTE EM TI
+- jovem-experiencia = display: none;
+- tecnico-experiencia = display: flex;
+
+
+*/
+var escolha = document.querySelector("#escolha");
+var ph = document.querySelector("#ph");
+var ep = document.querySelector("#ep");
+var habilidades = document.querySelector("#listaHabilidades");
+var experiencias = document.querySelector("#menuExperiencias");
+var jovem = document.querySelector("#jovemExperiencia");
+var tecnico = document.querySelector("#tecnicoExperiencia");
+var tj = document.querySelector("#tituloJovem");
+var tt = document.querySelector("#tituloTecnico");
+
+
+function mostrarHabilidades(){
+    escolha.style.display = "none";
+    habilidades.style.display = "flex";
+    experiencias.style.display = "none";
+    ph.style.cssText = "text-decoration: underline #390981";
+    ep.style.cssText = "text-decoration: none";
+}
+
+function mostrarExperiencias(){
+    escolha.style.display = "none";
+    habilidades.style.display = "none";
+    experiencias.style.display = "flex";
+    ph.style.cssText = "text-decoration: none";
+    ep.style.cssText = "text-decoration: underline #390981"; 
+}
+
+function mostrarJovem(){
+    jovem.style.display = "flex";
+    tecnico.style.display = "none";
+    tj.style.cssText = "text-decoration: underline #390981";
+    tt.style.cssText = "none";
+}
+function mostrarTecnico(){
+    jovem.style.display = "none";
+    tecnico.style.display = "flex";
+    tj.style.cssText = "text-decoration: none";
+    tt.style.cssText = "text-decoration: underline #390981";
+}
